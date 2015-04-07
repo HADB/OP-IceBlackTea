41f3c6615ad9883bf5cc6ba5b0f8febd1d5f5d67
Bean <Bean.B.Deng@newegg.com>
2015-04-07 11:25:52 +0800

    得分页面

--- Code/js/ibt.js
+++ Code/js/ibt.js
@@ -3,6 +3,8 @@
 
     effects: { moveUp: 1, moveDown: 2, fade: 3 },
 
+    results: { S: 1, A: 2, B: 3, C: 4, D: 5, E: 6, Failed: 0 },
+
     directions: { up: 1, down: -1 },
 
     isAnimating: false,
@@ -13,11 +15,16 @@
 
     game1end: false,
 
-    pageMove: function (effect, direction, pageCount) {
-        var nextPageNumber = IBT.currentPageNumber + direction * pageCount;
+    game1result: null,
+
+    resultReturnPageNumber: 1,
+
+    resultNextPageNumber: 1,
+
+    pageMove: function (effect, pageNumber) {
         var fromPage = ".page-" + IBT.currentPageNumber;
-        var toPage = ".page-" + nextPageNumber;
-        IBT.currentPageNumber = nextPageNumber;
+        var toPage = ".page-" + pageNumber;
+        IBT.currentPageNumber = pageNumber;
 
         switch (effect) {
             case IBT.effects.fade:
@@ -47,7 +54,7 @@
 
 $(function () {
     $(".game-1.page-1 .start").singleTap(function () {
-        IBT.pageMove(IBT.effects.fade, IBT.directions.up, 1);
+        IBT.pageMove(IBT.effects.fade, 2);
     });
 
     $(".game-1.page-2 .button-left,.game-1.page-2 .button-right").on('touchstart', function () {
@@ -67,34 +74,45 @@ $(function () {
     $(".game-1.page-2 .button-ok").singleTap(function () {
         IBT.game1end = true;
         if (IBT.game1ClickCount == 32) {
-            var level = "E";
             if (IBT.game1ClickUsingTime < 6.1) {
-                level = "S";
+                IBT.game1result = IBT.results.S;
             }
             else if (IBT.game1ClickUsingTime < 7.1) {
-                level = "A";
+                IBT.game1result = IBT.results.A;
             }
             else if (IBT.game1ClickUsingTime < 8.1) {
-                level = "B";
+                IBT.game1result = IBT.results.B;
             }
             else if (IBT.game1ClickUsingTime < 9.1) {
-                level = "C";
+                IBT.game1result = IBT.results.C;
             }
             else if (IBT.game1ClickUsingTime < 10.1) {
-                level = "D";
+                IBT.game1result = IBT.results.D;
             }
-            alert("耗时" + IBT.game1ClickUsingTime.toFixed(2) + "秒，等级为" + level);
+            else {
+                IBT.game1result = IBT.results.E;
+            }
+            showResult(IBT.game1result, IBT.game1ClickUsingTime.toFixed(2), 2, 4)
         }
         else {
-            alert(IBT.game1ClickUsingTime.toFixed(2) + "秒点击了" + IBT.game1ClickCount + "次，挑战失败！");
-            IBT.pageMove(IBT.effects.fade, IBT.directions.down, 1);
+            IBT.game1result = IBT.results.Failed;
+            alert("点击了" + IBT.game1ClickCount + "次，挑战失败！请重新进行挑战！");
+            IBT.pageMove(IBT.effects.fade, 1);
         }
         IBT.game1ClickCount = 0;
         IBT.game1ClickUsingTime = 0;
     });
+
+    $(".game-1.page-100 .button-return").singleTap(function () {
+        IBT.pageMove(IBT.effects.fade, IBT.resultReturnPageNumber);
+    });
+
+    $(".game-1.page-100 .button-next").singleTap(function () {
+        IBT.pageMove(IBT.effects.fade, IBT.resultNextPageNumber);
+    });
 });
 
-
+//游戏1计时器
 function startGame1Timer() {
     setTimeout(function () {
         if (!IBT.game1end) {
@@ -107,4 +125,44 @@ function startGame1Timer() {
             $(".game-1.page-2 .using-time").html("0.00");
         }
     }, 10);
+}
+
+//显示结果页面，目前不清楚4个游戏的结果页面是否通用，如果不通用，还需要进行一些修改
+function showResult(result, text, returnPageNumber, nextPageNumber) {
+    IBT.resultReturnPageNumber = returnPageNumber;
+    IBT.resultNextPageNumber = nextPageNumber;
+    IBT.pageMove(IBT.effects.fade, 100);
+    $(".game-1.page-100 .using-time").html(text + "s");
+    switch (result) {
+        case IBT.results.S:
+            $(".game-1.page-100 .result").attr("src", "img/score-s.png");
+            $(".game-1.page-100 .logo").addClass("right");
+            $(".game-1.page-100 .using-time").removeClass().addClass("using-time s");
+            break;
+        case IBT.results.A:
+            $(".game-1.page-100 .result").attr("src", "img/score-a.png");
+            $(".game-1.page-100 .logo").addClass("right");
+            $(".game-1.page-100 .using-time").removeClass().addClass("using-time a");
+            break;
+        case IBT.results.B:
+            $(".game-1.page-100 .result").attr("src", "img/score-b.png");
+            $(".game-1.page-100 .logo").addClass("right");
+            $(".game-1.page-100 .using-time").removeClass().addClass("using-time b");
+            break;
+        case IBT.results.C:
+            $(".game-1.page-100 .result").attr("src", "img/score-c.png");
+            $(".game-1.page-100 .logo").removeClass("right");
+            $(".game-1.page-100 .using-time").removeClass().addClass("using-time c");
+            break;
+        case IBT.results.D:
+            $(".game-1.page-100 .result").attr("src", "img/score-d.png");
+            $(".game-1.page-100 .logo").removeClass("right");
+            $(".game-1.page-100 .using-time").removeClass().addClass("using-time d");
+            break;
+        case IBT.results.E:
+            $(".game-1.page-100 .result").attr("src", "img/score-e.png");
+            $(".game-1.page-100 .logo").removeClass("right");
+            $(".game-1.page-100 .using-time").removeClass().addClass("using-time e");
+            break;
+    }
 }
